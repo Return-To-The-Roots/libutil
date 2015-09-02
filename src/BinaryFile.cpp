feda33c7861e171005dcfbd9a36c321e5cfc865c
Alexander Grund <Alex@grundis.de>
2015-09-02 19:19:14 +0200

    Remove svn headers

--- src/BinaryFile.cpp
+++ src/BinaryFile.cpp
@@ -1,5 +1,3 @@
-// $Id: BinaryFile.cpp 7093 2011-03-27 11:49:37Z OLiver $
-//
 // Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
