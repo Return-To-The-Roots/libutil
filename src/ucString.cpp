043580a2b57fba7e0a293866ebcf3a9436225eb2
Alexander Grund <Alex@grundis.de>
2016-03-10 22:57:05 +0100

    Add ucString for unicode strings

---
+++ src/ucString.cpp
@@ -0,0 +1,131 @@
+// Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
+#include "libUtilDefines.h" // IWYU pragma: keep
+#include "ucString.h"
+#include "../utf8/utf8.h"
+#include <boost/type_traits.hpp>
+#include <algorithm>
+
+// Include last!
+#include "DebugNew.h" // IWYU pragma: keep
+
+/// Decorator for an iterator that converts the elements to their unsigned equivalents
+/// Required because sign of char/wchar_t is undefined and we need it to be unsigned for some conversions
+template<class T_Container>
+struct GetUnsignedIterator{
+    typedef typename T_Container::const_iterator OldIterator;
+    typedef std::iterator_traits<OldIterator> oldTraits;
+    typedef typename oldTraits::value_type OldValueType;
+    typedef typename boost::make_unsigned<OldValueType>::type NewValueType;
+
+    class UnsignedIterator: public std::iterator<typename std::forward_iterator_tag, NewValueType>
+    {
+        OldIterator oldIt;
+    public:
+        UnsignedIterator(OldIterator it): oldIt(it) {}
+        UnsignedIterator(const UnsignedIterator& it): oldIt(it.oldIt) {}
+        UnsignedIterator& operator++() { ++oldIt; return *this; }
+        UnsignedIterator operator++(int) { UnsignedIterator old(*this); ++oldIt; return old; }
+        bool operator==(const UnsignedIterator& rhs) { return oldIt == rhs.oldIt; }
+        bool operator!=(const UnsignedIterator& rhs) { return oldIt != rhs.oldIt; }
+        NewValueType operator*() { return *oldIt; }
+    };
+};
+
+ucString cvUTF8ToUnicode(const std::string& other)
+{
+    ucString result;
+    typedef utf8::iterator<std::string::const_iterator> iterator;
+    result.assign(iterator(other.begin(), other.begin(), other.end()), iterator(other.end(), other.begin(), other.end()));
+    return result;
+}
+
+std::string cvUnicodeToUTF8(const ucString& other)
+{
+    std::string result;
+    result.reserve(other.length());
+    utf8::utf32to8(other.begin(), other.end(), std::back_inserter(result));
+    return result;
+}
+
+std::string cvWideStringToUTF8(const std::wstring& other)
+{
+    std::string result;
+    result.reserve(other.length());
+    try
+    {
+        // Try to convert from UTF-16 (UCS-2 is part of UTF-16)
+        utf8::utf16to8(other.begin(), other.end(), std::back_inserter(result));
+        return result;
+    } catch(...)
+    {
+        // If that fails, assume pure unicode
+        result.clear();
+        // Use unsigned elements to avoid wrong sign extension
+        typedef typename GetUnsignedIterator<std::wstring>::UnsignedIterator UIterator;
+        utf8::utf32to8(UIterator(other.begin()), UIterator(other.end()), std::back_inserter(result));
+        return result;
+    }
+}
+
+ucString cvWideStringToUnicode(const std::wstring& other)
+{
+    try
+    {
+        // Try to convert from UTF-16 (UCS-2 is part of UTF-16)
+        std::string tmp;
+        tmp.reserve(other.length());
+        utf8::utf16to8(other.begin(), other.end(), std::back_inserter(tmp));
+        return cvUTF8ToUnicode(tmp);
+    }
+    catch(...)
+    {
+        // If that fails, assume pure unicode
+        ucString result;
+        result.reserve(other.size());
+        // Use unsigned elements to avoid wrong sign extension
+        typedef typename GetUnsignedIterator<std::wstring>::UnsignedIterator UIterator;
+        result.assign(UIterator(other.begin()), UIterator(other.end()));
+        return result;
+    }
+}
+
+std::string cvStringToUTF8(const std::string& other)
+{
+    if(utf8::is_valid(other.begin(), other.end()))
+        return other;
+
+    std::string result;
+    result.reserve(other.length());
+    // Use unsigned elements to avoid wrong sign extension
+    typedef typename GetUnsignedIterator<std::string>::UnsignedIterator UIterator;
+    UIterator tmp(other.begin());
+    UIterator same = tmp++;
+    UIterator o = ++tmp;
+    utf8::utf32to8(UIterator(other.begin()), UIterator(other.end()), std::back_inserter(result));
+    return result;
+}
+
+ucString cvStringToUnicode(const std::string& other)
+{
+    ucString result;
+    // Use unsigned elements to avoid wrong sign extension
+    typedef typename GetUnsignedIterator<std::string>::UnsignedIterator UIterator;
+    result.assign(UIterator(other.begin()), UIterator(other.end()));
+    return result;
+}
---
+++ src/ucString.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef ucString_h__
+#define ucString_h__
+
+#include <string>
+#include <stdint.h>
+
+/// Typedef for storing and managing an Unicode (UTF-32) string
+typedef std::basic_string<uint32_t> ucString;
+
+//////////////////////////////////////////////////////////////////////////
+// Convert UTF8<->Unicode strings
+
+/// Convert an UTF-8 string to unicode string
+ucString cvUTF8ToUnicode(const std::string& other);
+
+/// Convert an unicode string to UTF-8 string
+std::string cvUnicodeToUTF8(const ucString& other);
+
+//////////////////////////////////////////////////////////////////////////
+// Legacy functions for wide strings (non-portable unicode versions)
+
+/// Convert a widestring (assumed to be UTF-16 or UCS-2) to unicode string
+std::string cvWideStringToUTF8(const std::wstring& other);
+/// Convert a widestring (assumed to be UTF-16 or UCS-2) to unicode string
+ucString cvWideStringToUnicode(const std::wstring& other);
+
+//////////////////////////////////////////////////////////////////////////
+// Functions that interpret the std::string as unicode
+// and simply convert 0x80-0xFF to U+80-U+FF
+// Note that this can badly fail as the source might be from a different
+// locale as the destination
+
+/// Convert a string (assumed to be codepage local) to UTF-8 unless it already is UTF-8
+std::string cvStringToUTF8(const std::string& other);
+/// Convert a string (assumed to be codepage local) to unicode string
+ucString cvStringToUnicode(const std::string& other);
+
+
+#endif // ucString_h__
