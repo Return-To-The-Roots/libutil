50d53737b74b38deb211445f863df0af92774515
Flamefire <git@grundis.de>
2018-07-10 20:18:15 +0200

    Make ENSURE_OUT_OF_SOURCE_BUILD execute immediately

--- cmake/Modules/CMakeMacroEnsureOutOfSourceBuild.cmake
+++ cmake/Modules/CMakeMacroEnsureOutOfSourceBuild.cmake
@@ -1,9 +1,6 @@
+# Ensures that the build dir is different to the source dir
 
-MACRO (ENSURE_OUT_OF_SOURCE_BUILD error)
-	STRING(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" source)
-
-	IF (source)
-		MESSAGE(SEND_ERROR "${error}")
-		MESSAGE(FATAL_ERROR "Remove the file CMakeCache.txt in ${CMAKE_SOURCE_DIR} first.")
-	ENDIF (source)
-ENDMACRO (ENSURE_OUT_OF_SOURCE_BUILD)
+if(CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+    message(SEND_ERROR "${PROJECT_NAME} requires an out of source build. Please create a separate build directory and run cmake there.")
+	message(FATAL_ERROR "Remove the file CMakeCache.txt in ${CMAKE_SOURCE_DIR} first.")
+endif()
