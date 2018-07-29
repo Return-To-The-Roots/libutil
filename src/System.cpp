3f5cc9b5b2e7073fa7370a53bdbd474828455318
Flow86 <github@fd.mytrap.de>
2018-07-29 07:36:08 +0200

    MAINT: do not rely on gcc version, instead simply check if REFKNOWNFOLDERID defined

--- src/System.cpp
+++ src/System.cpp
@@ -32,7 +32,7 @@
 #include <stdexcept>
 #endif // _WIN32
 
-#if defined(__MINGW32__) && (__GNUC__ < 6)
+#if defined(__MINGW32__) && !defined(REFKNOWNFOLDERID)
 
 typedef GUID KNOWNFOLDERID;
 #define REFKNOWNFOLDERID const KNOWNFOLDERID&
