18e9e99013a3131d6249d02fd8de3bd81bfc086c
Alexander Grund <alexander.grund@tu-dresden.de>
2018-12-11 16:50:14 +0100

    Add CheckAndAddFlag and EnableWarning modules

---
+++ cmake/Modules/CheckAndAddFlag.cmake
@@ -0,0 +1,44 @@
+include(CheckCXXCompilerFlag)
+include(CheckCCompilerFlag)
+
+macro(make_flag_for_check flag outVar)
+	# We cannot check for -Wno-foo as this won't throw a warning so we must check for the -Wfoo option directly
+	# http://stackoverflow.com/questions/38785168/cc1plus-unrecognized-command-line-option-warning-on-any-other-warning
+	string(REGEX REPLACE "^-Wno-" "-W" ${outVar} ${flag})
+endmacro()
+
+function(check_and_add_cxx_warning target visibility warningFlag)
+	make_flag_for_check(${warningFlag} escapedFlag)
+    # Remove special chars
+    string(MAKE_C_IDENTIFIER ${escapedFlag} VarName)
+	check_cxx_compiler_flag(${escapedFlag} CXX_WARNING_${VarName}_SUPPORTED)
+	if(CXX_WARNING_${VarName}_SUPPORTED)
+		target_compile_options(${target} ${visibility} ${warningFlag})
+	endif()
+endfunction()
+
+function(check_and_add_cxx_warnings target visibility)
+    foreach(warningFlag ${ARGN})
+        check_and_add_cxx_warning(${target} ${visibility} ${warningFlag})
+    endforeach()
+endfunction()
+
+function(CheckAndAddFlag flag)
+	make_flag_for_check(${flag} checkedFlag)
+    # Remove special chars
+    string(MAKE_C_IDENTIFIER ${checkedFlag} VarName)
+	check_cxx_compiler_flag(${checkedFlag} CXX_FLAG_${VarName}_SUPPORTED)
+	check_c_compiler_flag(${checkedFlag} C_FLAG_${VarName}_SUPPORTED)
+	if(CXX_FLAG_${VarName}_SUPPORTED)
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flag}" PARENT_SCOPE)
+	endif()
+	if(C_FLAG_${VarName}_SUPPORTED)
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${flag}" PARENT_SCOPE)
+	endif()
+endfunction()
+
+function(CheckAndAddFlags)
+    foreach(flag ${ARGN})
+        CheckAndAddFlag(${flag})
+    endforeach()
+endfunction()
---
+++ cmake/Modules/EnableWarnings.cmake
@@ -0,0 +1,62 @@
+option(RTTR_ENABLE_WERROR "Build with warnings turned into errors" ON)
+function(enable_warnings target)
+  get_target_property(targetType ${target} TYPE)
+  if(targetType STREQUAL "INTERFACE_LIBRARY")
+    set(visibility INTERFACE)
+  else()
+    set(visibility PUBLIC)
+  endif()
+  if(MSVC)
+    target_compile_options(${target} ${visibility}
+      /W3
+      /MP # parallel compilation
+      /w34062 # Enum not handled in switch
+      /w34389 # Signed/Unsigned operations
+      /wd4250 # 'class1' : inherits 'class2::member' via dominance (virtual inheritance related)
+      /wd4127 # conditional expr is constant
+      /wd4100 # unreferenced formal parameter
+      /wd4512 # assignment operator could not be created
+      /wd4267 # 'var' : conversion from 'size_t' to 'type', possible loss of data
+      /wd4250 # "use secure function"
+    )
+
+    target_compile_definitions(${target} ${visibility}
+      _CRT_SECURE_NO_WARNINGS _SCL_SECURE_NO_WARNINGS # disable "use secure function"
+      _CRT_NONSTDC_NO_DEPRECATE # disable MSVC posix functions
+      -D_WINSOCK_DEPRECATED_NO_WARNINGS
+    )
+    if(RTTR_ENABLE_WERROR)
+      target_compile_options(${target} ${visibility} /WX) # warning = error
+    endif()
+  else()
+    target_compile_options(${target} ${visibility} -Wall -Wextra -pedantic -Wpedantic)
+    if(RTTR_ENABLE_WERROR)
+      target_compile_options(${target} ${visibility} -Werror)
+    endif()
+    include(CheckAndAddFlag)
+    check_and_add_cxx_warnings(${target} ${visibility}
+      -Wparentheses
+      -Wno-error=type-limits
+      -Wfloat-conversion
+      -Wno-long-long
+      -Wno-deprecated-register
+      -Wno-unknown-pragmas
+      -fno-strict-aliasing
+      -Qunused-arguments
+    )
+
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang|GNU")
+      target_compile_options(${target} ${visibility}
+        # Variadic macros are part of C99 but supported by all big compilers in C++03
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<NOT:$<COMPILE_FEATURES:cxx_std_11>>>:-Wno-c++11-extensions;-Wno-variadic-macros;-Wno-c99-extensions>"
+        # For Boost < 1.59 (static-assert emulation)
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<NOT:$<COMPILE_FEATURES:cxx_static_assert>>>:-Wno-unused-local-typedef>"
+      )
+      if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 5.1)
+        target_compile_options(${target} ${visibility}
+          "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<COMPILE_FEATURES:cxx_override>>:-Wsuggest-override;-Wno-error=suggest-override>"
+        )
+      endif()
+    endif()
+  endif()
+endfunction()
