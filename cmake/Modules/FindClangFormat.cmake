4d0bf122eaed9e7a9c6056f80a63cfddec36009d
Flamefire <git@grundis.de>
2018-12-29 17:16:50 +0100

    Improve FindClangFormat
    Allow multiple files with same name
    More versions + better check
    Possibly faster

--- cmake/Modules/FindClangFormat.cmake
+++ cmake/Modules/FindClangFormat.cmake
@@ -15,20 +15,26 @@
 #       Allowed values for style are e.g.: LLVM, Google, Chromium, Mozilla, Webkit
 #       and file to use a .clang-format file in one of the srcs parent directory
 
+set(names clang-format)
+set(min 4)
+set(max 7)
+if(ClangFormat_FIND_VERSION_MAJOR)
+    set(min ${ClangFormat_FIND_VERSION_MAJOR})
+    if(max LESS ClangFormat_FIND_VERSION_MAJOR OR ClangFormat_FIND_VERSION_EXACT)
+        set(max ${ClangFormat_FIND_VERSION_MAJOR})
+    endif()
+endif()
+foreach(version RANGE ${min} ${max})
+    list(INSERT names 0 clang-format-${version}.0)
+endforeach()
+
 find_program(ClangFormat_BINARY
-    NAMES
-    clang-format-5.0
-    clang-format-4.0
-    clang-format-3.9
-    clang-format-3.8
-    clang-format-3.7
-    clang-format-3.6
-    clang-format
+    NAMES ${names}
     PATHS ${ClangTools_PATH} /usr/local/bin /usr/bin ENV CLANG_TOOLS_PATH
 )
 if(ClangFormat_BINARY)
-    execute_process(COMMAND ${ClangFormat_BINARY} --version OUTPUT_VARIABLE _out)
-    string(REGEX REPLACE ".*clang-format version ([^ \n]+).*" "\\1" ClangFormat_VERSION_STRING "${_out}")
+    execute_process(COMMAND ${ClangFormat_BINARY} --version OUTPUT_VARIABLE out)
+    string(REGEX REPLACE ".*clang-format version ([^ \n]+).*" "\\1" ClangFormat_VERSION_STRING "${out}")
 endif()
 
 include(FindPackageHandleStandardArgs)
@@ -47,44 +53,59 @@ if(ClangFormat_FOUND)
             set(ClangFormat_FILES ${ClangFormat_FILES} "${absCurFile}" CACHE INTERNAL "")
         endforeach()
     endfunction()
-    function(add_ClangFormat_folder _folder _recursive)
-        set(_tmpGLOB "")
-        string(REPLACE " " ";" _exts "${ClangFormat_SRC_EXTENSIONS}")
-        foreach(_ext ${_exts})
-            list(APPEND _tmpGLOB "${_folder}/${_ext}")
+    function(add_ClangFormat_folder folder recursive)
+        set(globExpr "")
+        string(REPLACE " " ";" exts "${ClangFormat_SRC_EXTENSIONS}")
+        foreach(ext ${exts})
+            list(APPEND globExpr "${folder}/${ext}")
         endforeach()
-        if(_recursive)
-            file(GLOB_RECURSE _NewClangFormatFiles ${_tmpGLOB})
+        if(recursive)
+            file(GLOB_RECURSE files ${globExpr})
         else()
-            file(GLOB _NewClangFormatFiles ${_tmpGLOB})
+            file(GLOB files ${globExpr})
         endif()
-        if(_NewClangFormatFiles)
-            add_ClangFormat_files(${_NewClangFormatFiles})
+        if(files)
+            add_ClangFormat_files(${files})
         endif()
     endfunction()
     function(print_ClangFormat_files)
         message(STATUS "Files for clang-format: ${ClangFormat_FILES}")
     endfunction()
-    function(add_ClangFormat_target _style)
+    function(add_ClangFormat_target style)
         if(ClangFormat_BINARY)
-            set(_format_files "")
-            set(_clangFormatFolder ${CMAKE_CURRENT_BINARY_DIR}/clangFormat)
-            file(MAKE_DIRECTORY ${_clangFormatFolder})
-            foreach (_source ${ClangFormat_FILES})
-                get_filename_component(_source_file ${_source} NAME)
-                set(_format_file ${_clangFormatFolder}/${_source_file}.format)
+            set(format_files "")
+            set(clangFormatFolder ${CMAKE_CURRENT_BINARY_DIR}/clangFormat)
+            # Cache last folder. To be more usefull sort the list first
+            list(SORT ClangFormat_FILES)
+            set(lastFolder "")
+            set(curClangFormatFolder ${clangFormatFolder})
+            foreach(source IN LISTS ClangFormat_FILES)
+                get_filename_component(source_file_name ${source} NAME)
+                get_filename_component(source_file_dir ${source} DIRECTORY)
+                # If the source folder has changed calculate the new curClangFormatFolder
+                if(NOT lastFolder STREQUAL source_file_dir)
+                    file(RELATIVE_PATH relDir ${CMAKE_SOURCE_DIR} ${source_file_dir})
+                    if(relDir MATCHES "\\.\\.")
+                        message(FATAL_ERROR "${source} is not in a sub directory of ${CMAKE_SOURCE_DIR}")
+                    endif()
+                    set(curClangFormatFolder ${clangFormatFolder}/${relDir})
+                    file(MAKE_DIRECTORY ${curClangFormatFolder})
+                    set(lastFolder ${source_file_dir})
+                endif()
+                set(format_file ${curClangFormatFolder}/${source_file_name}.format)
 
-                add_custom_command(OUTPUT ${_format_file}
-                        DEPENDS ${_source}
-                        COMMENT "Clang-Format ${_source}"
-                        COMMAND ${ClangFormat_BINARY} -style=${_style} -i ${_source}
-                        COMMAND ${CMAKE_COMMAND} -E touch ${_format_file})
+                add_custom_command(OUTPUT ${format_file}
+                        DEPENDS ${source}
+                        COMMENT "Clang-Format ${source}"
+                        COMMAND ${ClangFormat_BINARY} -style=${style} -i ${source}
+                        COMMAND ${CMAKE_COMMAND} -E touch ${format_file}
+                )
 
-                list(APPEND _format_files ${_format_file})
-            endforeach ()
+                list(APPEND format_files ${format_file})
+            endforeach()
      
-            if (_format_files)
-                add_custom_target(clangFormat DEPENDS ${_format_files})
+            if(format_files)
+                add_custom_target(clangFormat DEPENDS ${format_files})
             endif()
         endif()
     endfunction()
