6c7c9cfa9e71a9867809c007d2fc67ab2712924a
Flamefire <git@grundis.de>
2019-01-12 14:31:21 +0100

    Show testing status message only on initial configuration

--- cmake/RttrTestingCfg.cmake
+++ cmake/RttrTestingCfg.cmake
@@ -1,10 +1,14 @@
 # we do not use CTest because it will pull in a lot of unneeded targets
 # include(CTest)
 
-option(BUILD_TESTING "Build tests" ON)
-if(BUILD_TESTING)
-    message(STATUS "Testing ('make test') is enabled. Add -DBUILD_TESTING=OFF to disable tests.")
-    enable_testing()
-else()
-    message(STATUS "Testing ('make test') is disabled. Add -DBUILD_TESTING=ON to enable tests.")
+if(NOT DEFINED RTTR_TESTING_CFG)
+    set(RTTR_TESTING_CFG 1)
+
+    option(BUILD_TESTING "Build tests" ON)
+    if(BUILD_TESTING)
+        message(STATUS "Testing ('make test') is enabled. Add -DBUILD_TESTING=OFF to disable tests.")
+        enable_testing()
+    else()
+        message(STATUS "Testing ('make test') is disabled. Add -DBUILD_TESTING=ON to enable tests.")
+    endif()
 endif()
