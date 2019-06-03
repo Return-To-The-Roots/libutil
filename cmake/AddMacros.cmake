578576dc066047bfc8056cbf56e70594eaf09c01
Alexander Grund <alexander.grund@tu-dresden.de>
2019-06-03 13:40:12 +0200

    Fix linking of Boost.Test with CMake Config

--- cmake/AddMacros.cmake
+++ cmake/AddMacros.cmake
@@ -16,10 +16,11 @@ function(add_testcase)
     add_executable(${name} ${sources})
     target_link_libraries(${name} PRIVATE ${ARG_LIBS}
 		PRIVATE Boost::unit_test_framework
-	)
+    )
+    get_cmake_property(_variableNames VARIABLES)
     target_include_directories(${name} PRIVATE ${ARG_INCLUDES})
     # Heuristically guess if we are compiling against dynamic boost
-    if(NOT Boost_USE_STATIC_LIBS AND NOT Boost_UNIT_TEST_FRAMEWORK_LIBRARY MATCHES "\\${CMAKE_STATIC_LIBRARY_SUFFIX}\$")
+    if(NOT Boost_USE_STATIC_LIBS AND Boost_UNIT_TEST_FRAMEWORK_LIBRARY AND NOT Boost_UNIT_TEST_FRAMEWORK_LIBRARY MATCHES "\\${CMAKE_STATIC_LIBRARY_SUFFIX}\$")
         target_compile_definitions(${name} PRIVATE BOOST_TEST_DYN_LINK)
     endif()
     if(MSVC)
