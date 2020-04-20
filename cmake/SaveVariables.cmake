2187c5df75e5487fc6490132ee114bcb0d1d1c6f
Alexander Grund <git@grundis.de>
2020-04-20 10:24:26 +0200

    Build nowide with -fPIC

---
+++ cmake/SaveVariables.cmake
@@ -0,0 +1,26 @@
+# Save the variables passed as arguments and pushes their current values to a stack
+# Restore all those with pop_saved_variables()
+macro(save_variables)
+  if(NOT DEFINED _RTTR_SAVE_VARIABLES_COUNTER)
+    set(_RTTR_SAVE_VARIABLES_COUNTER 0)
+  endif()
+
+  math(EXPR _RTTR_SAVE_VARIABLES_COUNTER "${_RTTR_SAVE_VARIABLES_COUNTER}+1")
+  set(_RTTR_SAVE_VARIABLES_NAMES_${_RTTR_SAVE_VARIABLES_COUNTER} "")
+
+  foreach(var IN ITEMS ${ARGN})
+    set(_RTTR_SAVE_VARIABLES_OLD_${_RTTR_SAVE_VARIABLES_COUNTER}_${var} "${${var}}")
+    list(APPEND _RTTR_SAVE_VARIABLES_NAMES_${_RTTR_SAVE_VARIABLES_COUNTER} "${var}")
+  endforeach()
+endmacro()
+
+# Restore last recently saved variables
+macro(pop_saved_variables)
+  if(NOT _RTTR_SAVE_VARIABLES_COUNTER)
+    message(FATAL_ERROR "No saved variables")
+  endif()
+  foreach(var IN LISTS _RTTR_SAVE_VARIABLES_NAMES_${_RTTR_SAVE_VARIABLES_COUNTER})
+    set(${var} ${_RTTR_SAVE_VARIABLES_OLD_${_RTTR_SAVE_VARIABLES_COUNTER}_${var}})
+  endforeach()
+  math(EXPR _RTTR_SAVE_VARIABLES_COUNTER "${_RTTR_SAVE_VARIABLES_COUNTER}-1")
+endmacro()
