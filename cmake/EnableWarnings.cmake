170f53c856447c9ac10a95d81a07bb77d6fff47d
Alexander Grund <Flamefire@users.noreply.github.com>
2020-08-15 20:41:50 +0200

    Enable "unreferenced formal parameter" warning

--- cmake/EnableWarnings.cmake
+++ cmake/EnableWarnings.cmake
@@ -12,7 +12,6 @@ function(enable_warnings target)
       /MP # parallel compilation
       /w34062 # Enum not handled in switch
       /w34389 # Signed/Unsigned operations
-      /wd4100 # unreferenced formal parameter
       /wd4127 # conditional expr is constant
       /wd4250 # 'class1' : inherits 'class2::member' via dominance (virtual inheritance related)
       /wd4267 # 'var' : conversion from 'size_t' to 'type', possible loss of data
