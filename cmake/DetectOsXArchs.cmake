fec5da254498ecd9107245e76529f6763a8525f6
Alexander Grund <Flamefire@users.noreply.github.com>
2020-12-18 11:12:09 +0100

    Improve DetectOsXArchs and add fallback to x86

--- cmake/DetectOsXArchs.cmake
+++ cmake/DetectOsXArchs.cmake
@@ -4,52 +4,69 @@
 
 function(detect_osx_archs)
     find_library(LIB_SYSTEM_PATH NAMES libSystem.B.tbd libSystem.B.dylib)
-   	IF(NOT LIB_SYSTEM_PATH OR NOT EXISTS "${LIB_SYSTEM_PATH}")
-		MESSAGE(FATAL_ERROR "No libSystem.B.* found while trying to detect the architecture")
+    if(NOT LIB_SYSTEM_PATH OR NOT EXISTS "${LIB_SYSTEM_PATH}")
+        message(FATAL_ERROR "No libSystem.B.* found while trying to detect the architecture")
     endif()
-    MESSAGE(STATUS "Checking ${LIB_SYSTEM_PATH} for possible architectures")
-    
+    message(STATUS "Checking ${LIB_SYSTEM_PATH} for possible architectures")
+
     get_filename_component(LIBSYSTEM_EXT "${LIB_SYSTEM_PATH}" EXT)
     if(LIBSYSTEM_EXT STREQUAL ".B.dylib")
         # find lipo
         set(LIPO_NAMES apple-lipo lipo)
-        if(${CMAKE_CXX_COMPILER} MATCHES "^(.*)-[^-]+$" OR ${CMAKE_C_COMPILER} MATCHES "^(.*)-[^-]+$")
+        if(CMAKE_CXX_COMPILER MATCHES "^(.*)-[^-]+$" OR CMAKE_C_COMPILER MATCHES "^(.*)-[^-]+$")
             # Prefer lipo that has the same prefix as the compiler
             list(INSERT LIPO_NAMES 0 "${CMAKE_MATCH_1}-lipo")
         endif()
-		FIND_PROGRAM(LIPO NAMES ${LIPO_NAMES})
-        set(_cmd "${LIPO}")
-        set(_args -info)
-	ELSE()
-		# Newer OSX have only a text file *.tbd instead of the dylib file
-        set(_cmd grep)
-        set(_args archs)
-	ENDIF()
-    
-    # read supported platforms	
-    EXECUTE_PROCESS(
-        COMMAND "${_cmd}" ${_args} "${LIB_SYSTEM_PATH}"
-        RESULT_VARIABLE _RESULT
-        ERROR_VARIABLE _ERROR
-        OUTPUT_VARIABLE _OUTPUT
+        find_program(LIPO NAMES ${LIPO_NAMES})
+        set(cmd "${LIPO}")
+        set(args -info)
+    else()
+        # Newer OSX have only a text file *.tbd instead of the dylib file
+        set(cmd grep)
+        set(args archs)
+    endif()
+
+    # read supported platforms
+    execute_process(
+        COMMAND "${cmd}" ${args} "${LIB_SYSTEM_PATH}"
+        RESULT_VARIABLE cmdResult
+        ERROR_VARIABLE cmdStdErr
+        OUTPUT_VARIABLE cmdStdOut
     )
-    if(NOT _RESULT EQUAL 0)
-        message(FATAL_ERROR "Command '${_cmd}' failed with status ${_RESULT} and output: ${_ERROR}")
+    if(NOT cmdResult EQUAL 0 AND cmd STREQUAL "grep")
+        # Try again for newer XCode (seen on version 12)
+        execute_process(
+            COMMAND grep target "${LIB_SYSTEM_PATH}"
+            RESULT_VARIABLE cmdResult
+            ERROR_VARIABLE cmdStdErr
+            OUTPUT_VARIABLE cmdStdOut
+        )
+    endif()
+    if(NOT cmdResult EQUAL 0)
+        if(LIBSYSTEM_EXT STREQUAL ".B.tbd")
+            file(READ "${LIB_SYSTEM_PATH}" tbdContents)
+            set(tbdContents " TBD: ${tbdContents}")
+        else()
+            set(tbdContents "")
+        endif()
+        message(WARNING "Command '${cmd}' failed with status ${cmdResult} and output: ${cmdStdOut} ${cmdStdErr}"
+                        "Assuming at least x86_64 is supported.${tbdContents}")
+        set(cmdStdOut x86_64)
     endif()
 
     set(OSX_POSSIBLE_ARCHS x86_64 i386 i686 ppc)
-	set(OSX_DETECTED_ARCHS)
+    set(OSX_DETECTED_ARCHS)
     foreach(arch IN LISTS OSX_POSSIBLE_ARCHS)
-        IF(${_OUTPUT} MATCHES ${arch})
+        if(cmdStdOut MATCHES "${arch}")
             list(APPEND OSX_DETECTED_ARCHS ${arch})
-        ENDIF()
+        endif()
     endforeach()
-	
-	IF(NOT OSX_DETECTED_ARCHS)
-		MESSAGE(FATAL_ERROR "woooops: this should not happen! your system has no known architecture!")
-	ENDIF()
 
-	MESSAGE(STATUS "Possible architectures: ${OSX_DETECTED_ARCHS}")
+    if(NOT OSX_DETECTED_ARCHS)
+        message(FATAL_ERROR "woooops: this should not happen! your system has no known architecture!")
+    endif()
+
+    message(STATUS "Possible architectures: ${OSX_DETECTED_ARCHS}")
     set(OSX_POSSIBLE_ARCHS ${OSX_POSSIBLE_ARCHS} PARENT_SCOPE)
     set(OSX_DETECTED_ARCHS ${OSX_DETECTED_ARCHS} PARENT_SCOPE)
 endfunction()
