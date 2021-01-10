3dd184384286bb65924192e389cdf34893e4001d
Alexander Grund <Flamefire@users.noreply.github.com>
2021-01-10 20:53:25 +0100

    Add EnableCCache for use in submodules

---
+++ cmake/EnableCCache.cmake
@@ -0,0 +1,19 @@
+if(NOT MSVC)
+    #Check for ccache
+    if(APPLE)
+        find_program(CCACHE_PROGRAM NAMES ccache-lipo ccache)
+    else()
+        find_program(CCACHE_PROGRAM ccache)
+    endif()
+    mark_as_advanced(CCACHE_PROGRAM)
+    if(CCACHE_PROGRAM)
+        foreach(lang C CXX)
+            # Do only set ccache compiler if we don't already use a ccache wrapper
+            # Example: /usr/lib/ccache/g++
+            if(NOT CMAKE_${lang}_COMPILER MATCHES "ccache")
+                message(STATUS "Using ccache(${CCACHE_PROGRAM}) for ${lang} to speed up builds")
+                set(CMAKE_${lang}_COMPILER_LAUNCHER "${CCACHE_PROGRAM}")
+            endif()
+        endforeach()
+    endif()
+endif()
