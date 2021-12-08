db9b9e0f562dfea370987e170fe6fba95f8ade9e
Alexander Grund <Flamefire@users.noreply.github.com>
2021-12-08 19:00:01 +0100

    Use BOOST_TEST* macros instead of old variants
    Consistent use

--- tests/testTokenizer.cpp
+++ tests/testTokenizer.cpp
@@ -8,17 +8,17 @@
 BOOST_AUTO_TEST_CASE(Extract)
 {
     Tokenizer t("a1;b123;c1234 d12.f1", "; .");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.next() == "a1");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.next() == "b123");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.next() == "c1234");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.next() == "d12");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.next() == "f1");
-    BOOST_REQUIRE(!t);
+    BOOST_TEST_REQUIRE(!t);
     BOOST_TEST(t.next() == "");
     BOOST_TEST(t.next() == "");
 
@@ -26,16 +26,16 @@ BOOST_AUTO_TEST_CASE(Extract)
     for(const auto& singleStr : {"", "foobar", "b.c d"})
     {
         t = Tokenizer(singleStr, ";");
-        BOOST_REQUIRE(t);
+        BOOST_TEST_REQUIRE(t);
         BOOST_TEST(t.next() == singleStr);
-        BOOST_REQUIRE(!t);
+        BOOST_TEST_REQUIRE(!t);
     }
 }
 
 BOOST_AUTO_TEST_CASE(Explode)
 {
     Tokenizer t("a1;b123;c1234 d12.f1", "; .");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{"a1", "b123", "c1234", "d12", "f1"}),
                boost::test_tools::per_element());
 }
@@ -43,22 +43,22 @@ BOOST_AUTO_TEST_CASE(Explode)
 BOOST_AUTO_TEST_CASE(ExtractWithEmpty)
 {
     Tokenizer t("a;;c;;;f");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{"a", "", "c", "", "", "f"}), boost::test_tools::per_element());
 
     t = Tokenizer(";a;b");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{"", "a", "b"}), boost::test_tools::per_element());
 
     t = Tokenizer("a;b;");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{"a", "b", ""}), boost::test_tools::per_element());
 
     t = Tokenizer(";");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{"", ""}), boost::test_tools::per_element());
 
     t = Tokenizer("");
-    BOOST_REQUIRE(t);
+    BOOST_TEST_REQUIRE(t);
     BOOST_TEST(t.explode() == (std::vector<std::string>{""}), boost::test_tools::per_element());
 }
