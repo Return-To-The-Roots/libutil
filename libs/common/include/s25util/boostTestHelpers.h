423c6e343cf953e2c08aed2d93c865e2a087b2e3
Alexander Grund <Flamefire@users.noreply.github.com>
2021-12-11 16:04:20 +0100

    Add and test printing of smart pointers

--- libs/common/include/s25util/boostTestHelpers.h
+++ libs/common/include/s25util/boostTestHelpers.h
@@ -6,9 +6,24 @@
 #pragma once
 
 #include <boost/version.hpp>
+#include <iosfwd>
+#include <memory>
 
 #if BOOST_VERSION < 107000
 #    define BOOST_TEST_INFO_SCOPE(context_descr)                                             \
         ::boost::test_tools::tt_detail::context_frame BOOST_JOIN(context_frame_, __LINE__) = \
           ::boost::test_tools::tt_detail::context_frame(BOOST_TEST_LAZY_MSG(context_descr))
 #endif
+
+namespace std {
+template<class T, class D>
+std::ostream& boost_test_print_type(std::ostream& os, const std::unique_ptr<T, D>& p)
+{
+    return os << p.get();
+}
+template<class T>
+std::ostream& boost_test_print_type(std::ostream& os, const std::shared_ptr<T>& p)
+{
+    return os << p.get();
+}
+} // namespace std
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,24 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #define BOOST_TEST_MODULE s25util_Test
+#include "s25util/boostTestHelpers.h"
 #include <boost/test/unit_test.hpp>
+#include <memory>
+#include <sstream>
 
 //#include <vld.h>
+
+using TestedTypes = std::tuple<std::unique_ptr<int>, std::shared_ptr<int>>;
+BOOST_AUTO_TEST_CASE_TEMPLATE(PrintSmartPtr, T, TestedTypes)
+{
+    std::stringstream sOrig, sBoost;
+    T p;
+    sOrig << p.get();
+    boost_test_print_type(sBoost, p);
+
+    p.reset(new int(42));
+    sOrig << p.get();
+    boost_test_print_type(sBoost, p);
+
+    BOOST_TEST(sBoost.str() == sOrig.str());
+}
