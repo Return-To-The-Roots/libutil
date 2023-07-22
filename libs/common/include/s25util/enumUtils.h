82e2584b3054c1660340f7cc316486d2d4e6440c
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2023-07-22 17:04:58 +0200

    Add more bitset operators and trait-based opt-in
    Add new operator overloads to enhance the functionality of bitsets, and
    introduce a type trait that enables enums to opt in to being used as
    bitsets.

--- libs/common/include/s25util/enumUtils.h
+++ libs/common/include/s25util/enumUtils.h
@@ -6,16 +6,47 @@
 
 #include <type_traits>
 
+template<typename Enum>
+struct IsBitset : std::false_type
+{};
+
+template<typename Enum>
+constexpr bool IsValidBitset = IsBitset<Enum>::value && std::is_unsigned<std::underlying_type_t<Enum>>::value;
+
+template<typename Enum>
+using require_validBitset = std::enable_if_t<IsValidBitset<Enum>>;
+
+template<typename Enum, typename = require_validBitset<Enum>>
+constexpr auto operator&(Enum lhs, Enum rhs) noexcept
+{
+    using Int = std::underlying_type_t<Enum>;
+    return Enum(static_cast<Int>(lhs) & static_cast<Int>(rhs));
+}
+
+template<typename Enum, typename = require_validBitset<Enum>>
+constexpr auto operator|(Enum lhs, Enum rhs) noexcept
+{
+    using Int = std::underlying_type_t<Enum>;
+    return Enum(static_cast<Int>(lhs) | static_cast<Int>(rhs));
+}
+
+template<typename Enum, typename = require_validBitset<Enum>>
+constexpr auto operator&=(Enum& lhs, Enum rhs) noexcept
+{
+    return lhs = lhs & rhs;
+}
+
+template<typename Enum, typename = require_validBitset<Enum>>
+constexpr auto operator|=(Enum& lhs, Enum rhs) noexcept
+{
+    return lhs = lhs | rhs;
+}
+
 /// Makes a strongly typed enum usable as a bitset
-#define MAKE_BITSET_STRONG(Type)                                              \
-    inline auto operator&(Type lhs, Type rhs)                                 \
-    {                                                                         \
-        return Type(static_cast<unsigned>(lhs) & static_cast<unsigned>(rhs)); \
-    }                                                                         \
-    inline auto operator|(Type lhs, Type rhs)                                 \
-    {                                                                         \
-        return Type(static_cast<unsigned>(lhs) | static_cast<unsigned>(rhs)); \
-    }                                                                         \
-    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */                          \
-    static_assert(std::is_unsigned<std::underlying_type_t<Type>>::value,      \
+#define MAKE_BITSET_STRONG(Type)                                         \
+    template<>                                                           \
+    struct IsBitset<Type> : std::true_type                               \
+    {};                                                                  \
+    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */                     \
+    static_assert(std::is_unsigned<std::underlying_type_t<Type>>::value, \
                   #Type " must use unsigned type as the underlying type")
