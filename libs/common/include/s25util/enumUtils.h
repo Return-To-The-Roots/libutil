16908cbe8ed9deef10f0895642f4bcd5d31e7599
Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>
2023-08-05 13:44:28 +0200

    Modernize type traits

--- libs/common/include/s25util/enumUtils.h
+++ libs/common/include/s25util/enumUtils.h
@@ -11,7 +11,7 @@ struct IsBitset : std::false_type
 {};
 
 template<typename Enum>
-constexpr bool IsValidBitset_v = IsBitset<Enum>::value && std::is_unsigned<std::underlying_type_t<Enum>>::value;
+constexpr bool IsValidBitset_v = IsBitset<Enum>::value&& std::is_unsigned_v<std::underlying_type_t<Enum>>;
 
 template<typename Enum>
 using require_validBitset = std::enable_if_t<IsValidBitset_v<Enum>>;
@@ -71,10 +71,10 @@ template<typename Enum, typename = require_validBitset<Enum>>
 } // namespace bitset
 
 /// Makes a strongly typed enum usable as a bitset
-#define MAKE_BITSET_STRONG(Type)                                         \
-    template<>                                                           \
-    struct IsBitset<Type> : std::true_type                               \
-    {};                                                                  \
-    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */                     \
-    static_assert(std::is_unsigned<std::underlying_type_t<Type>>::value, \
+#define MAKE_BITSET_STRONG(Type)                                    \
+    template<>                                                      \
+    struct IsBitset<Type> : std::true_type                          \
+    {};                                                             \
+    /* NOLINTNEXTLINE(bugprone-macro-parentheses) */                \
+    static_assert(std::is_unsigned_v<std::underlying_type_t<Type>>, \
                   #Type " must use unsigned type as the underlying type")
