8d5ed0d8993eceb65c8a0e44af413cfd67900282
Alexander Grund <Flamefire@users.noreply.github.com>
2026-08-24 19:38:27 +0200

    Update message on which Boost is used
    The setting for static/dynamic Boost is enforced by the Boost CMake config files.
    So the "either static or dynamic" option is no longer applicable.

--- cmake/RttrBoostCfg.cmake
+++ cmake/RttrBoostCfg.cmake
@@ -1,11 +1,10 @@
-# Copyright (C) 2005 - 2021 Settlers Freaks <sf-team at siedler25.org>
+# Copyright (C) 2005 - 2026 Settlers Freaks <sf-team at siedler25.org>
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 #
 # Init RTTR_USE_STATIC_BOOST option
 # Show message about boost config
-# Add additional boost versions to Boost_ADDITIONAL_VERSIONS
 
 # Default boost libs are static on self-build windows versions and dynamic in the linux package repos
 if(WIN32 AND NOT MINGW)
@@ -15,17 +14,11 @@ else()
 endif()
 
 option(RTTR_USE_STATIC_BOOST "Build with static BOOST libraries instead of dynamic" ${RTTR_USE_STATIC_BOOST_DEFAULT})
-if((NOT Boost_USE_STATIC_LIBS AND RTTR_USE_STATIC_BOOST) OR
-   (Boost_USE_STATIC_LIBS AND NOT RTTR_USE_STATIC_BOOST))
-    unset(Boost_INCLUDE_DIR CACHE)
-    unset(Boost_LIBRARY_DIRS CACHE)
-endif()
 
-# Need to be explicitely set
 set(Boost_USE_STATIC_LIBS ${RTTR_USE_STATIC_BOOST})
 
 if(Boost_USE_STATIC_LIBS)
-   message(STATUS "Linking against boost static libraries")
+   message(STATUS "Linking against static boost libraries")
 else()
-   message(STATUS "Linking against boost dynamic or static libraries")
+   message(STATUS "Linking against dynamic boost libraries")
 endif()
